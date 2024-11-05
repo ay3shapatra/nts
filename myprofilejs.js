0dede99720343d47504344ac98e42b14c61d8289
ay3shapatra <ayeshabwc@gmail.com>
2024-11-05 19:02:58 +0530

    Add files via upload

---
+++ myprofilejs.js
@@ -0,0 +1,9 @@
+<script>
+// Get elements
+const changePasswordBtn = document.getElementById('change-password-btn');
+const connectDiscordBtn = document.getElementById('connect-discord-btn');
+
+// Handle change password button click
+changePasswordBtn.addEventListener('click', () => {
+// ... (handle password change logic)
+</script>
